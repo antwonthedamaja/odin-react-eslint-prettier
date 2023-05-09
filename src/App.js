d745b05941b56365ba90137c422363543481fd43
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-05-08 23:43:31 -0400

    Update file structure and boilerplates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import '../style/app.css'
+import '../style/App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+    return (
+      <div id="app">
+
+      </div>
+    );
 }
-
-export default App;
